fd38a2e6183db5bbc594f28dbadde97c092ce621
yukiroazn <yukiro.azn@live.com>
2023-03-20 16:09:32 +0100

    Import Project

---
+++ kasa/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar.jsx'
+import './App.css'
+import Banner from './components/Banner.jsx'
+
+function App() {
+return (
+
+<div>
+<Navbar/>
+<Banner/>
+</div>
+
+)
+}
+
+export default App
---
+++ kasa/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Navbar.css"
+
+function Navbar() {
+return ( 
+
+<nav className="navbar">
+ 
+<div className="navbarlogo">
+<img src="LOGO.svg" alt="logo"/>
+</div>
+
+<div className="acceuil">Acceuil</div>
+<div className="apropos">A propos</div>
+
+</nav>
+
+);
+}
+
+export default Navbar;
